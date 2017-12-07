77299337e0ad0abf74e90d9b23011d8992588beb
Richard B. Porter <richard-b-porter@uiowa.edu>
2017-12-07 14:18:34 -0600

    add list test case

---
+++ tests/ListTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace DrushUsersCommands\Tests;
+
+use DrushUsersCommands\Tests\TestBase;
+
+class ListTestCase extends TestBase
+{
+    /**
+     * Set up each test.
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->drush('role:create', ['editor'], $this->siteOptions);
+        $this->drush('user:create', ['foo'], $this->siteOptions);
+        $this->drush('user:create', ['bar'], $this->siteOptions);
+        $this->drush('user:block', ['bar'], $this->siteOptions);
+        $this->drush('user:role:add', ['editor', 'foo'], $this->siteOptions);
+    }
+
+    /**
+     * Test all users are returned.
+     */
+    public function testAllUsers()
+    {
+        $this->drush('users:list', [], $this->siteOptions);
+        $output = $this->getOutput();
+        $this->assertContains('foo', $output);
+        $this->assertContains('bar', $output);
+        $this->assertContains('admin', $output);
+    }
+
+    /**
+     * Test role option.
+     */
+    public function testUsersReturnedByRole()
+    {
+        $this->drush('users:list', [], $this->siteOptions + ['roles' => 'editor']);
+        $output = $this->getOutput();
+        $this->assertContains('foo', $output);
+        $this->assertNotContains('bar', $output);
+        $this->assertNotContains('admin', $output);
+    }
+
+    /**
+     * Test status option.
+     */
+    public function testUsersReturnedByStatus()
+    {
+        $this->drush('users:list', [], $this->siteOptions + ['status' => 'blocked']);
+        $output = $this->getOutput();
+        $this->assertNotContains('foo', $output);
+        $this->assertContains('bar', $output);
+        $this->assertNotContains('admin', $output);
+    }
+
+    /**
+     * Test both options in combination.
+     */
+    public function testUsersReturnedByBoth()
+    {
+        $this->drush('user:create', ['baz'], $this->siteOptions);
+        $this->drush('user:block', ['baz'], $this->siteOptions);
+        $this->drush('user:role:add', ['editor', 'baz'], $this->siteOptions);
+        $this->drush('users:list', [], $this->siteOptions + ['roles' => 'editor', 'status' => 'blocked']);
+        $output = $this->getOutput();
+        $this->assertNotContains('foo', $output);
+        $this->assertNotContains('bar', $output);
+        $this->assertNotContains('admin', $output);
+        $this->assertContains('baz', $output);
+    }
+
+    /**
+     * Test validation.
+     */
+    public function testValidation()
+    {
+        $result = $this->drush('users:list', [], $this->siteOptions + ['roles' => 2], null, null, self::EXIT_ERROR);
+        $this->assertEquals(1, $result);
+
+        $result = $this->drush('users:list', [], $this->siteOptions + ['roles' => null], null, null, self::EXIT_ERROR);
+        $this->assertEquals(1, $result);
+
+        $result = $this->drush('users:list', [], $this->siteOptions + ['status' => null], null, null, self::EXIT_ERROR);
+        $this->assertEquals(1, $result);
+
+        $result = $this->drush('users:list', [], $this->siteOptions + ['status' => 'foo'], null, null, self::EXIT_ERROR);
+        $this->assertEquals(1, $result);
+    }
+}
---
+++ tests/TestBase.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace DrushUsersCommands\Tests;
+
+use Unish\CommandUnishTestCase;
+
+abstract class TestBase extends CommandUnishTestCase
+{
+    public $siteOptions = [];
+    public $jsonOption = [];
+
+    public function setUp()
+    {
+        $sites = $this->setUpDrupal(1, true);
+        $site = key($sites);
+        $root = $this->webroot();
+
+        $this->siteOptions = [
+          'root' => $root,
+          'uri' => $site,
+          'yes' => null,
+          'include' => dirname(__DIR__),
+        ];
+
+        $this->jsonOption = [
+          'format' => 'json',
+        ];
+
+        $this->drush('cr', [], $this->siteOptions);
+    }
+}
