0651f496cac4c855ae1ebe60666ef5b071c9856b
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-06-01 11:16:16 -0500

    Add no-roles option test

--- tests/ListTest.php
+++ tests/ListTest.php
@@ -56,6 +56,31 @@ class ListTestCase extends TestBase
         $this->assertNotContains('admin', $output);
     }
 
+  /**
+   * Test no-role option.
+   */
+  public function testUsersReturnedByMultipleNoRoles()
+  {
+    $this->drush('role:create', ['publisher'], $this->siteOptions);
+    $this->drush('user:create', ['baz'], $this->siteOptions);
+    $this->drush('user:role:add', ['publisher', 'baz'], $this->siteOptions);
+
+    $this->drush('role:create', ['owner'], $this->siteOptions);
+    $this->drush('user:create', ['qux'], $this->siteOptions);
+    $this->drush('user:role:add', ['owner', 'qux'], $this->siteOptions);
+
+    $this->drush(
+      'users:list',
+      [],
+      $this->siteOptions + ['no-roles' => 'editor,publisher']
+    );
+
+    $output = $this->getOutput();
+    $this->assertContains('qux', $output);
+    $this->assertNotContains('foo', $output);
+    $this->assertNotContains('baz', $output);
+  }
+
     /**
      * Test status option.
      */
