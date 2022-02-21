7ed13efbd8725cd80e4e7f794687dff659bf75df
Richard B. Porter <richard-b-porter@uiowa.edu>
2022-02-21 14:51:31 -0600

    Remove test autoloader sut replacement

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,3 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-
-// Replace the SUT autoloader with ours.
-$content = "<?php return include __DIR__ . '/../../../autoload.php';";
-file_put_contents(__DIR__ . '/../vendor/drush/drush/sut/autoload.php', $content);
