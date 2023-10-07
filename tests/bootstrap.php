2a26f7816f69c6fa68254d15d5d3836ddbf63fe7
Richard B. Porter <rbp@richardbporter.com>
2023-10-06 20:27:43 -0500

    Write drush config in test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,4 +5,21 @@
  * Test bootstrap.
  */
 
+use Symfony\Component\Filesystem\Filesystem;
+
 require __DIR__ . '/../vendor/autoload.php';
+
+$fs = new Filesystem();
+$path = __DIR__ . '/../drush';
+$fs->mkdir($path);
+$fs->touch("$path/drush.yml");
+$workspace = getenv('GITHUB_WORKSPACE');
+
+$contents = <<<EOD
+# Register the command in the SUT.
+drush:
+  commands:
+    '\Drush\Commands\UsersCommands\UsersCommands': '$workspace/UsersCommands.php'
+EOD;
+
+file_put_contents("$path/drush.yml", $contents);
